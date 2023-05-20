4214cbff5c029e152d2c09af3f6d9faa6d01933e
jorenbroekema <joren.broekema@gmail.com>
2023-05-20 14:15:57 +0200

    fix bundler dynamic import var import assertions css

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,12 @@
 import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
 import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import { importAssertionsPlugin } from "rollup-plugin-import-assert";
 import { importAssertions } from "acorn-import-assertions";
+import css from "rollup-plugin-import-css";
 
 export default {
   input: "index.html",
   output: { dir: "dist" },
-  plugins: [
-    nodeResolve(),
-    importAssertionsPlugin(),
-    // FIXME: This dynamic import vars plugin doesn't seem to play well with import assertions..???
-    dynamicImportVars(),
-    html(),
-  ],
+  plugins: [nodeResolve(), css(), dynamicImportVars(), html()],
   acornInjectPlugins: [importAssertions],
 };
