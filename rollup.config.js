289e03b4a9f41f70181958c94e95c7e5fa23eac3
jorenbroekema <joren.broekema@gmail.com>
2023-05-20 14:44:20 +0200

    chore: use modules:true to always output CSSStyleSheet

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,18 @@
 import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
 import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import { importAssertions } from "acorn-import-assertions";
 import css from "rollup-plugin-import-css";
 
 export default {
   input: "index.html",
   output: { dir: "dist" },
-  plugins: [nodeResolve(), css(), dynamicImportVars(), html()],
-  acornInjectPlugins: [importAssertions],
+  plugins: [
+    nodeResolve(),
+    // see https://github.com/rollup/plugins/issues/1503
+    // if we put modules to true, we force CSSStyleSheet (rather than CSS String)
+    // Once the issue above is solved, the use of this option can be removed
+    css({ modules: true }),
+    dynamicImportVars(),
+    html(),
+  ],
 };
